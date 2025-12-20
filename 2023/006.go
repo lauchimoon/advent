5e93f38ecdee434b0a99edca2f0a53052932e0d2
lauchimoon <laurentino.luna06@gmail.com>
2025-12-20 12:18:46 -0300

    Solve 006 Part 2 from 2023

--- 2023/006.go
+++ 2023/006.go
@@ -32,23 +32,8 @@ func main() {
         distances = append(distances, x)
     }
 
-    total := 1
-    nRaces := len(times)
-
-    for i := 0; i < nRaces; i++ {
-        top := times[i]
-        waysToWin := 0
-        for buttonTime := int64(0); buttonTime <= top; buttonTime++ {
-            movement := buttonTime*(times[i] - buttonTime)
-            fmt.Printf("hold %v: %vmm\n", buttonTime, movement)
-            if movement > distances[i] {
-                waysToWin++
-            }
-        }
-
-        total *= waysToWin
-    }
-    fmt.Println(total)
+    fmt.Printf("Part 1: %d\n", Part1(times, distances))
+    fmt.Printf("Part 2: %d\n", Part2(times, distances))
 }
 
 func LoadStats(f io.Reader) []string {
@@ -62,4 +47,48 @@ func LoadStats(f io.Reader) []string {
     stats = append(stats, scanner.Text())
 
     return stats
+}
+
+func Part1(times, distances []int64) int {
+    total := 1
+    nRaces := len(times)
+
+    for i := 0; i < nRaces; i++ {
+        top := times[i]
+        waysToWin := 0
+        for buttonTime := int64(0); buttonTime <= top; buttonTime++ {
+            movement := buttonTime*(times[i] - buttonTime)
+            if movement > distances[i] {
+                waysToWin++
+            }
+        }
+
+        total *= waysToWin
+    }
+    return total
+}
+
+func Part2(times, distances []int64) int {
+    trueTime := ConcatenateSlice(times)
+    trueDistance := ConcatenateSlice(distances)
+
+    waysToWin := 0
+    for buttonTime := int64(0); buttonTime <= trueTime; buttonTime++ {
+        movement := buttonTime*(trueTime - buttonTime)
+        if movement > trueDistance {
+            waysToWin++
+        }
+    }
+
+    return waysToWin
+}
+
+func ConcatenateSlice(slice []int64) int64 {
+    sx := ""
+    for _, x := range slice {
+        sx += strconv.FormatInt(x, 10)
+    }
+    
+    v, _ := strconv.ParseInt(sx, 10, 64)
+    return v
 }
