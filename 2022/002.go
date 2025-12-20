1e0be4c78f849d75220081f3288e6affa6892459
lauchimoon <laurentino.luna06@gmail.com>
2025-12-20 19:37:01 -0300

    Solve 002 Part 2 from 2022

--- 2022/002.go
+++ 2022/002.go
@@ -18,7 +18,6 @@ const (
     ROCK_PLAYER    = "X"
     PAPER_PLAYER   = "Y"
     SCISSOR_PLAYER = "Z"
-
 )
 
 var (
@@ -33,59 +32,106 @@ func main() {
         log.Fatalf("failed to open file %s", FILEPATH)
     }
     defer f.Close()
-
-    total := 0
+    lines := []string{}
     scanner := bufio.NewScanner(f)
     for scanner.Scan() {
-        plays := strings.Fields(scanner.Text())
+        lines = append(lines, scanner.Text())
+    }
+
+    fmt.Printf("Part 1: %d\n", Part1(lines))
+    fmt.Printf("Part 2: %d\n", Part2(lines))
+}
+
+func Part1(lines []string) int {
+    total := 0
+    for _, line := range lines {
+        plays := strings.Fields(line)
         opponent := plays[0]
         player := plays[1]
         score := CalculateScore(opponent, player)
         total += score
     }
-    fmt.Println(total)
+    return total
 }
 
 func CalculateScore(opponent, player string) int {
     score := 0
-
     if opponent == ROCK_OPP {
         switch player {
-            case PAPER_PLAYER:
-                score = 6
-                break
-            case ROCK_PLAYER:
-                score = 3
-                break
-            case SCISSOR_PLAYER:
-                score = 0
-                break
+        case PAPER_PLAYER:
+            score = 6
+            break
+        case ROCK_PLAYER:
+            score = 3
+            break
+        case SCISSOR_PLAYER:
+            score = 0
+            break
         }
     } else if opponent == PAPER_OPP {
         switch player {
-            case PAPER_PLAYER:
-                score = 3
-                break
-            case ROCK_PLAYER:
-                score = 0
-                break
-            case SCISSOR_PLAYER:
-                score = 6
-                break
+        case PAPER_PLAYER:
+            score = 3
+            break
+        case ROCK_PLAYER:
+            score = 0
+            break
+        case SCISSOR_PLAYER:
+            score = 6
+            break
         }
     } else if opponent == SCISSOR_OPP {
         switch player {
-            case PAPER_PLAYER:
-                score = 0
-                break
-            case ROCK_PLAYER:
-                score = 6
-                break
-            case SCISSOR_PLAYER:
-                score = 3
-                break
+        case PAPER_PLAYER:
+            score = 0
+            break
+        case ROCK_PLAYER:
+            score = 6
+            break
+        case SCISSOR_PLAYER:
+            score = 3
+            break
         }
     }
 
     return score + shapePoints[player]
+}
+
+func Part2(lines []string) int {
+    total := 0
+    for _, line := range lines {
+        plays := strings.Fields(line)
+        opponent := plays[0]
+        player := DeterminePlayer(opponent, plays[1])
+        score := CalculateScore(opponent, player)
+        total += score
+    }
+    return total
+}
+
+func DeterminePlayer(opponent, end string) string {
+    if opponent == ROCK_OPP {
+        switch end {
+        case "X": return SCISSOR_PLAYER
+        case "Y": return ROCK_PLAYER
+        case "Z": return PAPER_PLAYER
+        default: return "?"
+        }
+    } else if opponent == PAPER_OPP {
+        switch end {
+        case "X": return ROCK_PLAYER
+        case "Y": return PAPER_PLAYER
+        case "Z": return SCISSOR_PLAYER
+        default: return "?"
+        }
+    } else if opponent == SCISSOR_OPP {
+        switch end {
+        case "X": return PAPER_PLAYER
+        case "Y": return SCISSOR_PLAYER
+        case "Z": return ROCK_PLAYER
+        default: return "?"
+        }
+    }
+
+    return "?"
 }
