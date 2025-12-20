e3a96e32d32475f8b138b8f1c6b55f6d7888915d
lauchimoon <laurentino.luna06@gmail.com>
2025-12-20 12:52:46 -0300

    Solve 015 Part 1 from 2023

---
+++ 2023/015.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+)
+
+const FILEPATH = "./resources/015input.txt"
+
+func main() {
+    f, err := os.Open(FILEPATH)
+    if err != nil {
+        log.Fatalf("failed to open file %s", FILEPATH)
+    }
+    defer f.Close()
+
+    scanner := bufio.NewScanner(f)
+    scanner.Scan()
+    src := scanner.Text()
+    total := 0
+
+    for _, s := range strings.Split(src, ",") {
+        total += HASH(s)
+    }
+    fmt.Println(total)
+}
+
+func HASH(s string) int {
+    h := 0
+    for _, c := range s {
+        h += int(c)
+        h *= 17
+        h %= 256
+    }
+    return h
+}
+
