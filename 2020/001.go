8f00dbc69f7af6a8294f9c2e54da64e38f9de01b
lauchimoon <laurentino.luna06@gmail.com>
2025-12-21 12:20:03 -0300

    Solve 001 Part 2 from 2020

--- 2020/001.go
+++ 2020/001.go
@@ -23,12 +23,30 @@ func main() {
         numbers = append(numbers, x)
     }
 
+    fmt.Printf("Part 1: %d\n", Part1(numbers))
+    fmt.Printf("Part 2: %d\n", Part2(numbers))
+}
+
+func Part1(numbers []int) int {
     for _, x := range numbers {
         for _, y := range numbers {
             if x + y == 2020 {
-                fmt.Println(x*y)
-                return
+                return x*y
             }
         }
     }
+    return -1
+}
+
+func Part2(numbers []int) int {
+    for _, x := range numbers {
+        for _, y := range numbers {
+            for _, z := range numbers {
+                if x + y + z == 2020 {
+                    return x*y*z
+                }
+            }
+        }
+    }
+    return -1
 }
