9359a8cd683ac12d97abc25b95fdbdb8c65065cc
lauchimoon <laurentino.luna06@gmail.com>
2025-12-21 12:37:14 -0300

    Solve 002 Part 2 from 2020

--- 2020/002.go
+++ 2020/002.go
@@ -25,27 +25,33 @@ func main() {
     }
 
     fmt.Printf("Part 1: %d\n", Part1(passwords))
+    fmt.Printf("Part 2: %d\n", Part2(passwords))
 }
 
 func Part1(passwords []string) int {
     total := 0
     for _, passwd := range passwords {
-        if IsValidPassword(passwd) {
+        if IsValidPassword(passwd, 1) {
             total++
         }
     }
     return total
 }
 
-func IsValidPassword(passwd string) bool {
+func IsValidPassword(passwd string, part int) bool {
     info := strings.Split(passwd, ": ")
     policy := strings.Fields(info[0])
     low, high := GetLetterTimes(policy[0])
-
     key := info[1]
-    letter := rune(policy[1][0])
-    letterCount := GetLetterCount(key, letter)
-    return letterCount >= low && letterCount <= high
+    letter := policy[1][0]
+
+    if part == 1 {
+        letterCount := GetLetterCount(key, letter)
+        return letterCount >= low && letterCount <= high
+    } else {
+        return (key[low - 1] == letter && key[high - 1] != letter) ||
+               (key[high - 1] == letter && key[low - 1] != letter)
+    }
 }
 
 func GetLetterTimes(letterRange string) (int, int) {
@@ -55,13 +61,23 @@ func GetLetterTimes(letterRange string) (int, int) {
     return low, high
 }
 
-func GetLetterCount(key string, letter rune) int {
+func GetLetterCount(key string, letter byte) int {
     count := 0
     for _, c := range key {
-        if c == letter {
+        if byte(c) == letter {
             count++
         }
     }
 
     return count
+}
+
+func Part2(passwords []string) int {
+    total := 0
+    for _, passwd := range passwords {
+        if IsValidPassword(passwd, 2) {
+            total++
+        }
+    }
+    return total
 }
