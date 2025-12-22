8d2cf2e9600effdebf661680709c56ab70699240
lauchimoon <laurentino.luna06@gmail.com>
2025-12-22 10:38:51 -0300

    Solve 009 Part 2 from 2020

--- 2020/009.go
+++ 2020/009.go
@@ -6,6 +6,7 @@ import (
     "log"
     "os"
     "strconv"
+    "sort"
 )
 
 const FILEPATH = "./resources/009input.txt"
@@ -24,6 +25,7 @@ func main() {
     }
 
     fmt.Printf("Part 1: %v\n", Part1(numbers))
+    fmt.Printf("Part 2: %v\n", Part2(numbers))
 }
 
 func Part1(numbers []int64) int64 {
@@ -34,6 +36,32 @@ func Part1(numbers []int64) int64 {
     }
 
     return numbers[i]
+}
+
+func Part2(numbers []int64) int64 {
+    target := int64(23278925)
+    sum := int64(0)
+    i := 0
+    startI := 0
+    for startI < len(numbers) {
+        if sum == target {
+            break
+        }
+        if i >= len(numbers) || sum > target  {
+            sum = 0
+            startI++
+            i = startI
+        }
+
+        sum += numbers[i]
+        i++
+    }
+
+    slice := numbers[startI:i]
+    sort.Slice(slice, func(i, j int) bool {
+        return slice[i] < slice[j]
+    })
+    return slice[0] + slice[len(slice)-1]
 }
 
 func IsSum(numbers []int64, x int64) bool {
