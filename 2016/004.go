97b8ae48aec12b2688c778eba2f97a964ab96e95
lauchimoon <laurentino.luna06@gmail.com>
2025-12-24 21:09:09 -0300

    Solve 004 Part 1 from 2016

---
+++ 2016/004.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"cmp"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"sort"
+	"slices"
+)
+
+type Pair struct {
+	First  rune
+	Second int
+}
+
+const FILEPATH = "./resources/004input.txt"
+
+func main() {
+    f, err := os.Open(FILEPATH)
+    if err != nil {
+        log.Fatalf("failed to open file %s", FILEPATH)
+    }
+    defer f.Close()
+
+	rooms := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		rooms = append(rooms, scanner.Text())
+	}
+
+	fmt.Println("Part 1:", Part1(rooms))
+}
+
+func Part1(rooms []string) int {
+	total := 0
+	for _, room := range rooms {
+		check, id := IsValidRoom(room)
+		if check {
+			total += id
+		}
+	}
+	return total
+}
+
+func IsValidRoom(room string) (bool, int) {
+	openBracketIdx := strings.Index(room, "[") + 1
+	checksum := room[openBracketIdx:openBracketIdx + 5]
+
+	info := strings.Split(room, "-")
+	id := stripId(info[len(info) - 1])
+
+	freqLetters := map[rune]int{}
+	for _, s := range info[:len(info) - 1] {
+		for _, c := range s {
+			freqLetters[c]++
+		}
+	}
+
+	fiveFrequent := []Pair{}
+	for k, v := range freqLetters {
+		fiveFrequent = append(fiveFrequent, Pair{k, v})
+	}
+
+	slices.SortFunc(fiveFrequent, func(a, b Pair) int {
+		if v := cmp.Compare(b.Second, a.Second); v != 0 {
+			return v
+		}
+		return cmp.Compare(a.First, b.First)
+	})
+
+	fiveFrequent = fiveFrequent[:5]
+	return ProduceChecksum(fiveFrequent) == SortString(checksum), id
+}
+
+func stripId(s string) int {
+	id, _ := strconv.Atoi(s[:strings.Index(s, "[")])
+	return id
+}
+
+func ProduceChecksum(frequent []Pair) string {
+	checksum := make([]rune, 5)
+	for i, p := range frequent {
+		checksum[i] = p.First
+	}
+
+	return SortString(string(checksum))
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+	return string(r)
+}
